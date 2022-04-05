a7ce85ccb6f1b6ebcf662d3b4c5b50d47d185903
zhuangzhuang <614962833@qq.com>
2022-04-05 12:03:42 +0800

    first

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+// import App from './App.vue'
+import App from '@/App'
+
+Vue.config.productionTip = false
+
+/* new Vue({
+  render: h => h(App),
+}).$mount('#app')
+ */
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
